6ca3cc6efaf300edf5ccb876b5585288045fde79
Juan Carlos Blandon Vargas <yoguisindevoz@gmail.com>
2020-04-12 14:16:35 -0500

    Add files via upload

---
+++ app/js/jsspellingYoguisBee.js
@@ -0,0 +1,177 @@
+
+var Bid = "Bid";
+var Ofertar = "Ofertar";
+var Apostar = "Apostar";
+var Rid = "Rid";
+var Deshacer = "Deshacer";
+var Spread = "Spread";
+var Sweat = "Sweat";
+var Beat = "Beat";
+var Hit = "Hit";
+var Fit = "Fit";
+var Quit = "Quit";
+var Split = "Split";
+var Slit = "Slit";
+var Bet = "Bet";
+var Let = "Let";
+var Set = "Set";
+var Wet = "Wet";
+var Cut = "Cut";
+var Put = "Put";
+var Shut = "Shut";
+var Thrust = "Thrust";
+var Burst = "Burst";
+var Hurt = "Hurt";
+var Cost = "Cost";
+var Cast = "Cast";
+var Broadcast = "Broadcast";
+var Forecast = "Forecast";
+var Read = "Read";
+var Lead = "Lead";
+
+var i_bid = [Bid, Bid, Bid];
+var regulares = ["a", "e"];
+var iregulares = [Bid, Rid];
+
+var btng1i = document.getElementById("btng1i");
+btng1i.addEventListener("click", g1i);
+
+var significadog1i = document.getElementById("significadog1i");
+var infinitivog1i = document.getElementById("infinitivog1i");
+var pasadog1i = document.getElementById("pasadog1i");
+var participiog1i = document.getElementById("participiog1i");
+
+var significadog1i1 = document.getElementById("significadog1i1");
+var infinitivog1i1 = document.getElementById("infinitivog1i1");
+var pasadog1i1 = document.getElementById("pasadog1i1");
+var participiog1i1 = document.getElementById("participiog1i1");
+
+function g1i(evt)
+{
+	significadog1i.innerHTML = Ofertar;
+	infinitivog1i.innerHTML = Bid;
+	pasadog1i.innerHTML = Bid;
+	participiog1i.innerHTML = Bid;
+	
+	significadog1i1.innerHTML = Deshacer;
+	infinitivog1i1.innerHTML = Rid;
+	pasadog1i1.innerHTML = Rid;
+	participiog1i1.innerHTML = Rid;	
+}
+
+var btng2i = document.getElementById("btng2i");
+btng2i.addEventListener("click", g2i);
+
+var significadog1i0 = document.getElementById("significadog1i0");
+var infinitivog1i0 = document.getElementById("infinitivog1i0");
+var pasadog1i0 = document.getElementById("pasadog1i0");
+var participiog1i0 = document.getElementById("participiog1i0");
+
+var significadog1i11 = document.getElementById("significadog1i11");
+var infinitivog1i11 = document.getElementById("infinitivog1i11");
+var pasadog1i11= document.getElementById("pasadog1i11");
+var participiog1i11 = document.getElementById("participiog1i11");
+
+var significadog1i2 = document.getElementById("significadog1i2");
+var infinitivog1i2 = document.getElementById("infinitivog1i2");
+var pasadog1i2 = document.getElementById("pasadog1i2");
+var participiog1i2 = document.getElementById("participiog1i2");
+
+
+function g2i(evt)
+{
+	significadog1i0.innerHTML = Hit;
+	infinitivog1i0.innerHTML = Hit;
+	pasadog1i0.innerHTML = Hit;
+	participiog1i0.innerHTML = Hit;
+	
+	significadog1i11.innerHTML = Fit;
+	infinitivog1i11.innerHTML = Fit;
+	pasadog1i11.innerHTML = Fit;
+	participiog1i11.innerHTML = Fit;
+	
+	significadog1i2.innerHTML = Quit;
+	infinitivog1i2.innerHTML = Quit;
+	pasadog1i2.innerHTML = Quit;
+	participiog1i2.innerHTML = Quit;
+}
+
+
+
+/*
+var btng1i = document.getElementById("btng1i");
+
+var btnr = document.getElementById("btnregular");
+var btnir = document.getElementById("btniregular");
+var btng1ir = document.getElementById("btniregular7");
+
+btng1i.addEventListener("click", g1i);
+
+btnr.addEventListener("click", reg);
+btnir.addEventListener("click", ireg);
+btng1ir.addEventListener("click", g1);
+
+var infinitivog1i = document.getElementById("infinitivog1i");
+
+var resultado = document.getElementById("resultado");
+var resultado1 = document.getElementById("resultado1");
+var resultado_espanol = document.getElementById("resultado_espanol");
+
+
+function reg(evt)
+{
+	infinitivog1.innerHTML = Bid;
+}
+
+
+
+function reg(evt)
+{
+	resultado.innerHTML = i_bid;
+}
+
+
+function ireg(evt)
+{
+	resultado1.innerHTML = iregulares;
+}
+
+*/
+
+
+
+//Mostrar5
+//1. alert(iregulares);
+//2. console.log(iregulares);
+//3. document.write(iregulares);
+//4. document.getElementById("resultado").innerHTML = iregulares;
+/* 
+5.
+var resultado = document.getElementById("resultado");
+resultado.innerHTML = iregulares;
+*/
+
+/* funciona
+function reg(evt)
+{
+	document.getElementById("resultado").innerHTML = regulares;
+}
+
+
+function ireg(evt)
+{
+	document.getElementById("resultado1").innerHTML = iregulares;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
