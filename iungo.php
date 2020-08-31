8efaede70c58d6e75a0ad8cd01297a647f2ad750
Juan Carlos Blandon Vargas <yoguisindevoz@gmail.com>
2020-08-30 22:56:37 -0500

    Add files via upload

---
+++ iungo.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Los Yoguis Fase 1..</title>
+	<link rel="icon" href="img/Fovicon/Yoguis3.ico">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">	
+</head>
+<body>
+	<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution="setup_tool"
+  page_id="235460526501940">
+</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-3">	
+			<nav class="nav fixed-top bg-dark d-flex justify-content-center">			
+				<a href="index.php" class="nav-link">Los Yoguis</a>
+				<a href="Fase_1.php" class="nav-link">Fase I</a>
+				<a href="Fase_2.php" class="nav-link">Fase II</a>
+		   	    <a href="Fase_3.php" class="nav-link">Fase III</a>
+				<a href="Fase_4.php" class="nav-link">Fase IV</a>
+				<a href="otras/Capacitacion.php" class="nav-link">Cursos</a>
+				<a href="SaladeCine.php" class="nav-link">Sala de Cine</a>
+				<a href="Emisora.php" class="nav-link">Emisora</a>
+				<a href="apoyo/gracias.php" class="nav-link">Apoyo</a>
+				<a href="apoyo/staff.php" class="nav-link">Staff</a>
+				<a href="portafolio/Autor.php" class="nav-link">Facilitador</a>
+			</nav>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<div class="card">
+					<div class="card-body">
+						<h1 class="mb-3">IUNGO</h1>
+						<p>	Programación por Bloques </p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col">
+				<!-- 21:9 .embed-responsive-21by9 -->
+				<!-- 16:9 .embed-responsive-16by9 -->
+				<!-- 4:3 .embed-responsive-4by3 -->
+				<!-- 1:1 .embed-responsive-1by1 -->
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/IXYQfGwwk3M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+							</div>
+		</div>
+		
+		<div>.</div>
+		
+		<div class="row">
+			<div class="col">
+				<!-- 21:9 .embed-responsive-21by9 -->
+				<!-- 16:9 .embed-responsive-16by9 -->
+				<!-- 4:3 .embed-responsive-4by3 -->
+				<!-- 1:1 .embed-responsive-1by1 -->
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/SZeRLnZtxGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+							</div>
+		</div>
+		
+		
+
+		<footer class="row">
+			<div class="col">
+				
+				<h4>Programación por Bloques</h4>
+				<p>2019 - 2020</p>
+			</div>
+		</footer>
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
