1b6ccb846b9e56117b3f30b6514f2c38522796f8
Juan Carlos Blandon Vargas <yoguisindevoz@gmail.com>
2020-08-30 22:56:17 -0500

    Add files via upload

---
+++ lectura.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Los Yoguis Fase 1..</title>
+	<link rel="icon" href="img/Fovicon/Yoguis3.ico">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">	
+</head>
+<body>
+	<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution="setup_tool"
+  page_id="235460526501940">
+</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-3">	
+			<nav class="nav fixed-top bg-dark d-flex justify-content-center">			
+				<a href="index.php" class="nav-link">Los Yoguis</a>
+				<a href="Fase_1.php" class="nav-link">Fase I</a>
+				<a href="Fase_2.php" class="nav-link">Fase II</a>
+		   	    <a href="Fase_3.php" class="nav-link">Fase III</a>
+				<a href="Fase_4.php" class="nav-link">Fase IV</a>
+				<a href="otras/Capacitacion.php" class="nav-link">Cursos</a>
+				<a href="SaladeCine.php" class="nav-link">Sala de Cine</a>
+				<a href="Emisora.php" class="nav-link">Emisora</a>
+				<a href="apoyo/gracias.php" class="nav-link">Apoyo</a>
+				<a href="apoyo/staff.php" class="nav-link">Staff</a>
+				<a href="portafolio/Autor.php" class="nav-link">Facilitador</a>
+			</nav>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<div class="card">
+					<div class="card-body">
+						<h1 class="mb-3">Lectura y Escritura Digital</h1>
+						<p>	6º 7º </p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+
+		<div class="row mt-3">
+			
+			
+			
+			<div class="col">
+				<div class="card text-white bg-success">
+					<div class="card-body">
+						<h3 class="card-title">Experiencia significativa en lectura y escritura digital "Los Yoguis"</h3>
+						<h4 class="card-title">I.E Manuel J. Betancur</h4>
+						<p></p>
+						<h4 class="card-title">Preproducción</h4>
+						
+						<!--<a href="#" class="btn btn-dark">Ir al post</a>-->
+				
+					</div>
+				</div>
+				
+			
+				
+				<div class="card-body">
+						
+						<p> 
+						   <p> 
+							<li>
+								<a href="https://docs.google.com/document/d/1bAIomDAhoB6jrukuGq-1p9YSvTCsIPf0MxUk9o4oA8A/edit?usp=sharing" 	target="_blank">Proyecto</a>
+							</li>
+							<li>
+								<a href="https://manuelj.gnomio.com/course/view.php?id=457" target="_blank">Plataforma Moodle I.E Manuel J. Betancur - Aula Iconografica</a>
+							</li>
+							<li>
+								<a href="https://manuelj.gnomio.com/course/index.php?categoryid=47" target="_blank">Escritos Bodas de Oro I.E MJB - 2015 -</a> </li>
+							<li>
+								<a href="https://plataformamanueljb.wixsite.com/bodasdeoromjb?=&idmenutipo=3152&tag=" target="_blank">Productos audiovisuales bodas de oro I.E MJB 6º 7º 8º - 2015 -</a>
+							</li>
+							<li>
+								<a href="https://docs.google.com/spreadsheets/d/1BqjeolyWuBkTDrjhrFQonTEWPO9DcR6n_t4AaecGo8k/edit?usp=sharing" target="_blank">Escritos 2016</a>
+							</li>
+							<li>
+								<a href="https://docs.google.com/spreadsheets/d/10AxvRUw48QOS8LpyF-86rGJo8g3-55eBhjLl-Nc21OI/edit?usp=sharing" target="_blank">Escritos 2017</a>
+							</li>
+													
+							<li>
+								<a href="https://docs.google.com/spreadsheets/d/1Lk6s9lkbWX1P3OsiwNzPKfPHuR3ChP24Cj9o9-N_T18/edit?usp=sharing" target="_blank">Escritos 2018</a>
+							</li>
+					
+							<li>
+								<a href="https://manuelj.gnomio.com/course/view.php?id=415" target="_blank">Swaycuentos 7º - Temática: cátedra de la paz 2018 </a>
+							</li>
+							
+							<li>
+								<a href="https://manuelj.gnomio.com/" target="_blank">Escritos 2019 - En construcción - </a>
+							</li>
+					
+							<li>
+								<a href="https://manuelj.gnomio.com/course/view.php?id=415" target="_blank">Swaycuentos 7º - Temática: cátedra de la paz 2019 - En construcción</a>
+							</li>
+							
+						</p>
+							
+						</p>
+						
+				
+					
+				
+		
+			
+			
+
+		<footer class="row">
+			<div class="col">
+				
+				<h4>Lectura y escritura digital</h4>
+				<p>2020</p>
+			</div>
+		</footer>
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
